7827dd7691c41b795259ed69762124de074624e4
MichalakJ <daran.dasaner@gmail.com>
2016-06-09 18:42:47 +0200

    Implemented LoginPage class

---
+++ src/test/java/edu/iis/mto/bdd/cucumber/workflowsteps/LoginPage.java
@@ -0,0 +1,23 @@
+package edu.iis.mto.bdd.cucumber.workflowsteps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	private final WebDriver driver;
+	
+	public LoginPage(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public void open(){
+		driver.get("http://localhost:8080/#/welcome");
+		
+	}
+	public void signInWithCredentials(String userEmail, String userPassword){
+		driver.findElement(By.name("email")).sendKeys(userEmail);
+		driver.findElement(By.name("password")).sendKeys(userPassword);
+		driver.findElement(By.name("signin")).click();
+	}
+
+}
